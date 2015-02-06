e934a45dbafa632fc83876fcf00d957d92f5c8d8
Panayiotis Lipiridis <lipiridis@gmail.com>
2015-02-06 22:37:24 +0100

    Fixed conflicts

--- main/model/pay.py
+++ main/model/pay.py
@@ -4,14 +4,15 @@ from __future__ import absolute_import
 
 from google.appengine.ext import ndb
 
+from api import fields
 import config
 import model
 import util
 
 class Pay(model.Base):
   name = ndb.StringProperty(default='')
-  date_for = ndb.DateProperty(auto_now_add=True)
-  date_paid = ndb.DateProperty(auto_now_add=True)
+  date_for = ndb.DateTimeProperty(auto_now_add=True)
+  date_paid = ndb.DateTimeProperty(auto_now_add=True)
   code = ndb.StringProperty(default='')
   amount = ndb.FloatProperty(default=0.0)
 
@@ -23,19 +24,22 @@ class Pay(model.Base):
   def is_positive(self):
     return self.amount >= 0
 
-  _PROPERTIES = model.Base._PROPERTIES.union({
-      'amount',
-      'amount_format',
-      'code',
-      'date_for',
-      'date_paid',
-      'is_positive',
-      'name',
-    })
-
   @classmethod
   def get_dbs(cls, is_positive=None, **kwargs):
     return super(Pay, cls).get_dbs(
         is_positive=is_positive or util.param('is_positive', bool),
         **kwargs
       )
+
+
+PAY_FIELDS = {
+  'amount': fields.Float,
+  'amount_format': fields.String,
+  'code': fields.String,
+  'date_for': fields.DateTimeField,
+  'date_paid': fields.DateTimeField,
+  'is_positive': fields.Boolean,
+  'name': fields.String,
+}
+
+PAY_FIELDS.update(model.BASE_FIELDS)
